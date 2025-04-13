7909dde7bbfd58262a9a2eb44de50671cbab45a8
Gabin Noubissi <gnoubissi@gmx.de>
2025-04-13 15:10:51 +0200

    version pre php

--- JS/main.js
+++ JS/main.js
@@ -26,39 +26,6 @@ const myinterval = setInterval(changeImage, 3000);
 // Activer la première barre au démarrage
 progressBars[index].classList.add("active");
 
-// =================================== Currentslide show and stop automatic caroussel=============================================//
-
-function currentSlide(i) {
-
-    // Stop automatic mode
-    clearInterval(myinterval);
-
-    progressBars.forEach(bar => bar.classList.remove("active"));
-    progressBars.forEach(bar => bar.classList.remove("active__after_clicked"));
-
-    slides.forEach(slide => slide.style.display = "none");
-    index = i;
-    progressBars[index].classList.add("active__after_clicked");
-    slides[index].style.display = "block";
-
-}
-// =======================================  Activate and desactivate drop-down menu  ==========================================//
-
-const navLinks = document.querySelector(".nav__links");
-const menuBtn = document.getElementById("menu__icon");
-// const menuBtnClose = document.getElementById("menu__icon-close");
-menuBtn.addEventListener("click", function() {
-    navLinks.classList.toggle("active");
-    menuBtn.innerHTML = navLinks.classList.contains("active") ? "✖" : "☰";
-}
-)
-
-document.addEventListener("click", function (event) {
-    if (!navLinks.contains(event.target) && !menuBtn.contains(event.target)) {
-        navLinks.classList.remove("active");
-        menuBtn.textContent = "☰";
-    }
-})
 
 // =======================================  Activate and desactivate category  ==========================================//
 // Sélection de la barre de progression
@@ -76,9 +43,7 @@ function currentCat(index) {
     // Déplacer la barre progressive sous le bouton sélectionné
     const test = document.querySelector('.categories__item:nth-child(1)');
     const breite = test.offsetWidth;
-    console.log(breite);
-    // const style = window.getComputedStyle(element);
-    // const breite = style.getPropertyValue('width');
+
     const index_cat = (index-2) % 4;
     // pb.style.transform = `translateX(${index * 100}%)`;
     pb.style.transform = `translateX(${index_cat * breite}px)`;
@@ -92,4 +57,3 @@ function currentCat(index) {
                 .join("");
     
 }
-
